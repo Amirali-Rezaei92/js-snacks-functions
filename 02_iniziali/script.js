db7ade0e5331a6edb49f36a12dcbed1e6be64682
Amirali Rezaei <amiralirezaie169@gmail.com>
2026-04-01 15:55:09 +0200

    i writen Function for snack 2.
    ho scritto Funzione per snack2.

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+const prendiIniziali = (names) => {
+    const iniziali = [];
+    for (let i = 0; i < names.length; i++) {
+        const nome = names[i];
+        const iniziale = nome[0];
+        iniziali.push(iniziale);
+    }
+    return iniziali;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
 
+
+console.log(prendiIniziali(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
