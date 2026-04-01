362cdb003a7e06c13d9c0d4e67bce354d001a928
Amirali Rezaei <amiralirezaie169@gmail.com>
2026-04-01 15:51:31 +0200

    i writen Function for snack1
    ho scritto Funzione per snack1

---
+++ 01_saluti/script.js
@@ -0,0 +1,16 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+
+
+// Dichiara la funzione qui.
+const Saluta = (userName) => {
+    return `Ciao ${userName}`;
+};
+
+// Invoca la funzione qui e stampa il risultato in console
+
+Saluta();
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
+console.log(Saluta(userName));
