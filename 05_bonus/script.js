7acffbbd2ff43b825d014d91237011ed1db2da5a
Amirali Rezaei <amiralirezaie169@gmail.com>
2026-04-01 17:49:12 +0200

    i writen function for bonus file.
    ho scritto Funzione per Bonus.

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,27 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+const nome = 'Mario';
 
 
 // Dichiara la funzione qui.
 
+const saluta = nome =>{
+    let message = '';
+    const ora = new Date().getHours();
+    if(ora <13){
+        message = `Buongiorno ${nome}`;
+    }else if(ora<17){
+        message = `Buon pomeriggio ${nome}`;
+    }else {
+        message = `Sera ${nome}`;
+    }
+    return message;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(saluta(nome));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
