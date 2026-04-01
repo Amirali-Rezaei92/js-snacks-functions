880de2ee2b69810bb93cc1dcb4056da9c4c6baac
Amirali Rezaei <amiralirezaie169@gmail.com>
2026-04-01 17:28:49 +0200

    i writen Function for snack 4.
    ho scritto Funzione per snack4.

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,20 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+const contaVocali = word =>{
+    let count = 0;
+    for(i=0 ; i<word.length ; i++){
+        let vocale = word[i].toLowerCase();
+        if(vocale === 'a' || vocale === 'e' || vocale === 'o' || vocale === 'i' || vocale === 'u'){
+            count++;
+        }
+    }
+    return count;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(contaVocali(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
